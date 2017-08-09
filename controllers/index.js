548fb8d900a71a90e968a3b97ca8e61127267436
goobering <goobering@gmail.com>
2017-08-09 11:46:12 +0100

    Built out app.js with methods for populating from countries API.

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,6 @@ var express = require('express');
 var router = new express.Router();
 var path = require('path');
 
-// router.use('/api/coffee', require('./coffee.js'));
-
 router.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/../client/build/index.html'));
 });
